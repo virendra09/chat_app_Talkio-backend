5898e5f8fa2ddc1fd72ccfe7c7f1ba9d0fb48f81
Virendra Yadav <virendrayadav1292000@gmail.com>
2025-06-23 18:46:15 +0530

    fix the auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -77,7 +77,12 @@ export const login = async (req, res) => {
 
 export const logout = (req, res) => {
   try {
-    res.cookie("jwt", "", { maxAge: 0 });
+    res.cookie("jwt", "", { 
+      maxAge: 0,
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.log("Error in logout controller", error.message);
